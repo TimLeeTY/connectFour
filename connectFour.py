acc8ea7e511bff8a531e26e9b91b3b841de3dc0c
Tim Lee <timlee@tyl35c.pem.private.cam.ac.uk>
2018-03-16 11:52:38 +0000

    added biases to activation and fixed win detection criteria

--- connectFour.py
+++ connectFour.py
@@ -1,6 +1,4 @@
 import numpy as np
-import time
-import multiprocessing as mp
 
 
 class board:
@@ -8,7 +6,6 @@ class board:
     def __init__(self, dim=[7, 6]):
         self.dim = dim
         self.state = np.zeros(dim)
-        self.finished = False
 
     def checkWin(self, play, row):
         colSum = self.state[:, row].cumsum()
@@ -24,17 +21,14 @@ class board:
 
     def move(self, play, player):
         self.moves += 1
-        t0 = time.clock()
+        if self.winner != 0:
+            return
         if self.state[play, -1] != 0 or play > self.dim[0]:
             raise NameError('invalid move')
-        for row in range(self.dim[1]):
-            t1 = time.clock()
-            if self.state[play, row] == 0:
-                self.state[play, row] = player
-                if self.checkWin(play, row):
-                    self.finished = True
-                    self.winner = player
-                break
+        row = np.argwhere(self.state[play, :] == 0)[0]
+        self.state[play, row] = player
+        if self.checkWin(play, row):
+            self.winner = player
 
     def printState(self):
         for row in np.flip(self.state.T, 0):
@@ -44,50 +38,46 @@ class board:
 
     def playAvA(self, a1, a2):
         self.state = np.zeros(self.dim)
-        self.finished = False
         self.moves = 0
-        while self.moves < self.dim[0]*self.dim[1] and not self.finished:
+        self.winner = 0
+        while self.moves < self.dim[0]*self.dim[1] and self.winner == 0:
             self.move(a1.makeMove(self), 1)
             self.move(a2.makeMove(self), -1)
-        if not self.finished:
-            self.winner = 0
         return(self.winner)
 
     def playHvH(self):
         self.state = np.zeros(self.dim)
-        self.finished = False
         self.moves = 0
+        self.winner = 0
         self.printState()
-        while not (self.finished) and self.moves < self.dim[0]*self.dim[1]:
-            while not (self.finished):
+        while self.moves < self.dim[0]*self.dim[1] and self.winner == 0:
+            while self.winner == 0:
                 try:
                     self.move(int(input('player 1\'s turn, select row from 0...%i: ' % (self.dim[0] - 1))), 1)
                     self.printState()
                     break
                 except (NameError, ValueError, IndexError):
                     print('invalid move, try again')
-            while not (self.finished):
+            while self.winner == 0:
                 try:
                     self.move(int(input('player 2\'s turn, select row from 0...%i: ' % (self.dim[0] - 1))), -1)
                     self.printState()
                     break
                 except (NameError, ValueError, IndexError):
                     print('invalid move, try again')
-        if not self.finished:
-            self.winner = 0
+        if self.winner == 0:
             print('draw')
-            self.printState()
         else:
             print('player %i won' % {1: 1, -1: 2}[self.winner])
         return(self.winner)
 
     def playHvA(self, a):
         self.state = np.zeros(self.dim)
-        self.finished = False
         self.moves = 0
+        self.winner = 0
         self.printState()
-        while not (self.finished) and self.moves < self.dim[0]*self.dim[1]:
-            while not (self.finished):
+        while self.moves < self.dim[0]*self.dim[1] and self.winner == 0:
+            while self.winner == 0:
                 try:
                     self.move(int(input('player\'s turn, select row from 0...%i: ' % (self.dim[0] - 1))), 1)
                     self.printState()
@@ -96,8 +86,7 @@ class board:
                     print('invalid move, try again')
             self.move(a.makeMove(self), -1)
             self.printState()
-        if not self.finished:
-            self.winner = 0
+        if self.winner == 0:
             print('draw')
             self.printState()
         else:
@@ -110,11 +99,13 @@ class agent:
     def __init__(self, layer, board):
         layer = np.concatenate(([board.dim[0] * board.dim[1]], layer, [board.dim[0]]))
         self.w = np.array([np.random.rand(layer[i], layer[i+1])*2-1 for i in range(len(layer)-1)])
+        self.bias = [np.random.rand(i)-0.5 for i in layer[1:]]
 
     def makeMove(self, board):
         prob = board.state.flatten()
         for i in range(len(self.w)):
-            prob = 1/(np.exp(-1 * np.dot(prob, self.w[i])) + 1)     # Apply sigmoid
+            # Apply sigmoid after applying bias
+            prob = 1/(np.exp(-1 * np.dot(prob, self.w[i])+self.bias[i]) + 1)
         prob[np.nonzero(board.state[:, -1])] = 0
         if np.sum(prob) != 0:
             prob = prob/np.sum(prob)
@@ -141,7 +132,8 @@ def compete(agents):
     return(ret)
 
 
-def generation(mutateProb, scoreSort, agentArr):
+def generation(mutateProb, agentScore, agentArr):
+    scoreSort = agentScore.argsort()[-15:][::-1]
     agentPairs = np.array([[agentArr[scoreSort[i]], agentArr[scoreSort[j]]]
                            for i in range(len(scoreSort)) for j in range(i)])
     for pair in range(len(agentPairs)):
@@ -151,14 +143,16 @@ def generation(mutateProb, scoreSort, agentArr):
             break
         if not (np.array_equal(a1.w[0], a2.w[0]) and np.array_equal(a1.w[1], a2.w[1])):
             for i in range(len(a1.w)):
-                a1.w[i] = (a1.w[i] + a2.w[i]) / 2
+                a1.w[i] = np.where(np.random.randint(2, size=a1.w[i].shape), a1.w[i], a2.w[i])
+                + np.random.normal(0, 0.05, a1.w[i].shape)      # Randomly select traits from parents, add some mutation
+                a1.bias[i] = np.where(np.random.randint(2, size=a1.bias[i].shape), a1.bias[i], a2.bias[i])
+                + np.random.normal(0, 0.05, a1.bias[i].shape)   # Randomly select traits from parents, add some mutation
         agentArr[pair] = a1
     return(agentArr)
 
 
 board_1 = board()
 a = 105
-t0 = time.clock()
 agentArr = [agent([10, 7], board_1) for i in range(a)]
 for gens in range(30):
     mutateProb = 1 / (2+gens)
@@ -166,12 +160,11 @@ for gens in range(30):
     agentScore = np.zeros(a)
     agentMat = [[[agentArr[i], agentArr[j]] for j in range(a)] for i in range(a)]
     out = np.array(list(map(compete, agentMat)))
-    print(out.shape)
     agentScore = out.sum(axis=0) - out.sum(axis=1)
-    scoreSort = agentScore.argsort()[-15:][::-1]
-    agentArr = generation(mutateProb, scoreSort, agentArr)
-t1 = time.clock()
-print(t1 - t0)
+    agentArr = generation(mutateProb, agentScore, agentArr)
+agentMat = [[[agentArr[i], agentArr[j]] for j in range(a)] for i in range(a)]
+out = np.array(list(map(compete, agentMat)))
+agentScore = out.sum(axis=0) - out.sum(axis=1)
 print(agentScore, np.sum(agentScore))
 while(True):
     board_1.playHvA(agentArr[agentScore.argmax()])
